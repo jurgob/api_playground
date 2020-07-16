661d03a2afdb46b1e4115bb86c76925a1afe2e19
Jurgo Boemo <jurgo.boemo@gmail.com>
2020-07-16 22:36:24 +0100

    add some intro

--- src/App.js
+++ src/App.js
@@ -10,7 +10,15 @@ function App() {
           API Playground
         </p>
       </header>
-      <EventPanel />
+      <div className="App-content">
+        <p className="App-intro" >
+          This is a playground where you can run api request in sequence. <br />
+          in every request you can specify an <i>id</i> <br />
+          the request is specified in the <i>req</i> property, it use the <a href="https://github.com/axios/axios#request-config">axios format</a> <br/>
+          you can futfill your request using previous request responses using  {"{{}}"}. every response is stored in <i>responses.REQ_ID</i>.  <br />
+        </p>
+        <EventPanel />
+      </div>
     </div>
   );
 }
